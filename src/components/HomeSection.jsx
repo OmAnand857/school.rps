dc4b98804a01ab63e26f181d804aa9682801d9b0
OmAnand857 <allansmith2561@gmail.com>
2024-05-23 15:09:30 +0530

    ..

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -11,7 +11,7 @@ function HomeSection(){
                                     <img class="w-full h-[75vh]" src="./homeBanner.png" alt=""></img>
 
 
-                                <div  class=" w-[80vw] lg:w-[40vw] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] md:top-[30%] md:left-[15%] md:translate-y-[-50%]">
+                                <div  class=" w-[80vw] lg:w-[40vw] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] lg:top-[40%] lg:left-[40%] ">
                                     <p class=" text-[5vw] md:text-[4vw] lg:text-[2vw] font-[Roboto] text-white">Welcome to</p>
                                     <h1 class="text-[10vw] text-white font-bold mb-[1vw] md:text-[8vw] lg:text-[4vw] font-[Outfit] leading-[100%]">Royal Public <br/> School <span class="text-[8vw] text-[#FE9132]">,</span> Raipur</h1>
                                     <div class='flex  text-white gap-[3vw]'><button class="bg-[#1B7242] p-[1vw] rounded">Get Started</button> <button class="bg-transparent border-white border-2 p-[1vw] rounded">Watch Video</button></div>
